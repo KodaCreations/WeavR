b2199be5cee11c9b8f1c9eeaafdcdc78b0b20a3d
Andreas Karlsson <andreas@kodacreations.se>
2016-11-04 13:04:12 +0100

    Added base trigger and effect class and some child classes

---
+++ Assets/Scripts/LevelScripts/Triggers/BasicTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Trigger that activates when intersected
+/// </summary>
+public class BasicTrigger : Trigger {
+
+    public void OnTriggerEnter()
+    {
+        activate = true;
+    }
+}
---
+++ Assets/Scripts/LevelScripts/Triggers/Trigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Base trigger class
+/// </summary>
+public class Trigger : MonoBehaviour {
+
+    // Reference to affected object in scene
+    public TriggerEffect teObject;              
+    protected bool activate = false;
+
+    public virtual void Update()
+    {
+        // Activates the effect
+        if (activate == true)
+            teObject.Activate();
+    }
+}
