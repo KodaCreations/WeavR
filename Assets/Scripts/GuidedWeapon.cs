f3975cf06114f2e94b1ec2f09bd6b729b25071a1
M09K3730 <david90dahl@yahoo.se>
2016-11-07 15:32:06 +0100

    Very basic weponry

---
+++ Assets/Scripts/GuidedWeapon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuidedWeapon : Weapon {
+    public ShipController target;
+    private float speed;
+
+	// Use this for initialization
+	public override void Start () {
+        Initialize(5);
+        base.Start();
+	}
+
+    private void Initialize(float speed)
+    {
+        this.speed = speed;
+    }
+	
+	// Update is called once per frame
+	public override void Update () {
+        if (target)
+        {
+            Vector3 dir = target.transform.position - transform.position;
+
+            transform.position += dir * Time.deltaTime * speed;
+
+            base.Update();
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        ParticleSystem explosion = GetComponent<ParticleSystem>();
+        explosion.Play();
+        Destroy(gameObject, explosion.duration);
+        GetComponent<Collider>().enabled = false;
+
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+	
+	}
+}
