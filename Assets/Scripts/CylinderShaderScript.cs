578fc32b8bf3681ed9fc30f1d107d2095f1a6e42
Ossibo <card-gamer@hotmail.com>
2016-11-17 12:40:39 +0100

    Started working on Flight Mode

---
+++ Assets/Scripts/CylinderShaderScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderShaderScript : MonoBehaviour {
+    GameObject ship;
+	// Use this for initialization
+	void Start () {
+        //ship = GameObject.Find("PhysicsTestShip(Clone)");
+        //ship.GetComponent<Renderer>().material.SetVector("Ship Pos", new Vector4(ship.transform.position.x, ship.transform.position.y, ship.transform.position.z, 0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(!ship)
+        {
+            ship = GameObject.Find("PhysicsTestShip(Clone)");
+        }
+        if(ship)
+        {
+            Debug.Log("Ship Found!");
+            ship.GetComponent<Renderer>().material.SetVector("_ShipPos", new Vector4(ship.transform.position.x, ship.transform.position.y, ship.transform.position.z, 0));
+        }
+	}
+}
