10d489d271e1bd94bad41dfb50b843887f855329
Axel Hornay <Axel.Hornay@gmail.com>
2016-11-28 14:40:53 +0100

    Base for AI

---
+++ Assets/Scripts/AIScripts/EditorPath.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EditorPath : MonoBehaviour {
+
+    public Color Rcolor = Color.red;
+    public List<Transform> pathObjs = new List<Transform>();
+    Transform[] pathArray;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Rcolor;
+        pathArray = GetComponentsInChildren<Transform>();
+        pathObjs.Clear();
+
+        foreach (Transform pathObj in pathArray)
+        {
+            if (pathObj != this.transform)
+            {
+                pathObjs.Add(pathObj);
+            }
+        }
+
+        for (int i = 0; i < pathObjs.Count; i++)
+        {
+            Vector3 position = pathObjs[i].position;
+            if (i > 0)
+            {
+                Vector3 previousPos = pathObjs[i - 1].position;
+                Gizmos.DrawLine(previousPos, position);
+                Gizmos.DrawWireSphere(position, 0.3f);
+            }
+        }
+    }
+}
