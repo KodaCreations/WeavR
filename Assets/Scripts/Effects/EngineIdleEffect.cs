f270197f811a3084c2a90d86440e3f1dc86ce5e1
Andreas Karlsson <andreas@kodacreations.se>
2016-12-19 01:48:19 +0100

    Added new test scene for ship prefab work

---
+++ Assets/Scripts/Effects/EngineIdleEffect.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineIdleEffect : MonoBehaviour {
+
+    public float frequency;
+    public float scaleChangeValue;
+    public float scaleRange;
+    public Transform shipTransform;
+    public float offsetX;
+    public float offsetY; 
+    public float offsetZ;
+
+    private Vector3 offsetValues;
+    private float maxScale;
+    private float minScale;
+    private float currentScale;
+    private float timer;
+    private float startScale;
+
+	// Use this for initialization
+	void Start () {
+
+        startScale = transform.localScale.x;
+        currentScale = startScale;        
+        maxScale = currentScale + scaleRange;
+        minScale = currentScale - scaleRange;
+        timer = frequency;
+        offsetValues = new Vector3(offsetX, offsetY, offsetZ);
+	}
+
+    void ChangeScale()
+    {
+        if (currentScale >= startScale)
+        {
+            if (currentScale >= maxScale)
+            {
+                currentScale -= scaleChangeValue;
+            }
+            else
+            {
+                if (Random.Range(1, 3) != 1)
+                {
+                    currentScale -= scaleChangeValue;
+                }
+                else
+                {
+                    currentScale += scaleChangeValue;
+                }
+            }
+        }
+        else
+        {
+
+            if (currentScale <= minScale)
+            {
+                currentScale += scaleChangeValue;
+            }
+            else
+            {
+                if (Random.Range(1, 3) != 1)
+                {
+                    currentScale += scaleChangeValue;
+                }
+                else
+                {
+                    currentScale -= scaleChangeValue;
+                }
+            }
+        }
+
+        transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.localPosition = shipTransform.localPosition + offsetValues;
+
+        if(timer <= 0)
+        {
+            ChangeScale();
+            timer = frequency;
+        }
+        timer -= Time.deltaTime;
+
+	}
+}
---
+++ Assets/Scripts/Effects/EngineParticleEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineParticleEffect : MonoBehaviour {
+
+    public Transform shipTransform;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+    private Vector3 offsetValues;
+
+	// Use this for initialization
+	void Start () {
+        offsetValues = new Vector3(offsetX, offsetY, offsetZ);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = shipTransform.localPosition + offsetValues;
+	}
+}
